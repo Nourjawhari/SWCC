f1ca3a97354cd43a96cf842042feef9e1c968013
Nourjawhari <141036230+Nourjawhari@users.noreply.github.com>
2023-08-24 10:04:06 +0300

    Add files via upload

---
+++ SWCC_Translation.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    // Fetching the current URL
+    currentURL = window.location.href;
+
+    // Default Language Keyword is set to RuntimeAR
+    const runtimeKeyword = "RuntimeAR";
+
+    // Setting the language globally
+    if (currentURL.includes(runtimeKeyword)) {
+        currentLanguage = "AR"
+    } else {
+        currentLanguage = "EN"
+    }
+
+    // Changing the language to english
+    $("[name='EnglishFlag']").on("click", function () {
+        updateURL("EnglishFlag");
+    });
+
+    // Changing the language to arabic
+    $("[name='ArabicFlag']").on("click", function () {
+        updateURL("ArabicFlag");
+    });
+});
+
+ 
+
+// Function to update the URL based on flag clicks
+function updateURL(keyword) {
+    let newURL;
+
+    // Switching to English Runtime
+    if (keyword === "EnglishFlag") {
+        if (currentURL.includes("RuntimeAR")) {
+            newURL = currentURL.replace("RuntimeAR", "Runtime");
+        }
+
+    // Switching to Arabic Runtime
+    } else if (keyword === "ArabicFlag") {
+        if (!currentURL.includes("RuntimeAR")) {
+            newURL = currentURL.replace("Runtime", "RuntimeAR");
+        }
+    }
+
+    // Change the URL and immediately go to the new URL in the same tab
+    if (newURL) {
+        window.location.replace(newURL);
+    }
+}
+
+function langIsAr(){
+    return currentLanguage == "AR"
+}
